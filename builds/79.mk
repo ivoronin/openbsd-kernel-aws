e3a52a1b228379fe3cac52dfe0cfcb3fd792b83b
Ilya Voronin <ivoronin@octalwave.com>
2026-07-08 09:52:15 +0300

    Pin ENA driver revision

--- builds/79.mk
+++ builds/79.mk
@@ -3,7 +3,7 @@ ERRATA_URL = https://cdn.openbsd.org/pub/OpenBSD/patches/7.9/common
 PATCHES_PATH = patches/79
 SIGNIFY_KEY = keys/openbsd-79-base.pub
 DRIVERS = \
-	https://github.com/ivoronin/openbsd-driver-ena@6cdcedad22285145318c198472fb31a3157efe81:src \
+	https://github.com/ivoronin/openbsd-driver-ena@43087a112dbbf5643f1f00155a0c9f657330e930:src \
 	https://github.com/ivoronin/openbsd-driver-plgpio-acpi@fe8de2ce32076d36c88678fd0cd14e99d8dad831:src
 
 ISO_CHECKSUM.amd64 = sha256:7a4a92e953618035097c796a90b54424a0f3ae775552e1e7d102cf8a5130449f
